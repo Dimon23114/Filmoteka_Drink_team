68f0e8ae50d3a7cbb8f3d46fd89bd59ef88dcca9
NazarBezpaliuk <100237989+NazarBezpaliuk@users.noreply.github.com>
2022-09-29 21:08:17 +0000

    Deploying to gh-pages from @ Dimon23114/Filmoteka_Drink_team@b576fc558e5ccc8dbab1f061002930b983d720a5 🚀

--- index.85e83354.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".header__form"),document.querySelector(".header-form__input"),document.querySelector(".backdrop"),document.querySelector("modal__close-btn"),document.querySelector(".modal__info-box"),document.querySelector(".modal__choice-btn--watched"),document.querySelector(".modal__choice-btn--queue");
-//# sourceMappingURL=index.85e83354.js.map
